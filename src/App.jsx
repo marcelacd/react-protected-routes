b7dede6815f26834f1641fe8c5748588f879edae
Marcela Carmona <gloriamarcela.carmonaduran@emeal.nttdata.com>
2024-02-20 22:41:09 -0500

    Crear estructura de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import StateLogin from "./useContext/loginContext/StateLogin";
+import { Admin, Analytics, Dashboard, Home, Landing } from "./pages";
 
 function App() {
-  return <StateLogin>HOLA</StateLogin>;
+  Navigation();
+
+  return (
+    <StateLogin>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route index element={<Landing />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+    </StateLogin>
+  );
 }
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/landing">Loading</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link to="/Analytics">Analytics</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link to="/admin">Admin</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default App;
