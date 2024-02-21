527c225627bb0ba2065ac56e1acfb234b5d12e5d
Marcela Carmona <gloriamarcela.carmonaduran@emeal.nttdata.com>
2024-02-20 22:19:51 -0500

    Eliminar componentes no utilizados

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+import StateLogin from "./useContext/loginContext/StateLogin";
+
+function App() {
+  return <StateLogin>HOLA</StateLogin>;
+}
+
+export default App;
