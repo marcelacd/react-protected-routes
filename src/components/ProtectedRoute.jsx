2ed3b43e789c094f928d8f2c92288acff91909eb
Marcela Carmona <gloriamarcela.carmonaduran@emeal.nttdata.com>
2024-03-07 21:34:15 -0500

    Probando

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,6 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 export const ProtectedRoute = ({ isAllowed, children, redirectTo = "/landing" }) => {
   if (!isAllowed) {
     return <Navigate to={redirectTo} />;
+    
   }
 
   return children ? children : <Outlet />;
